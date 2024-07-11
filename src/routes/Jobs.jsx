f6a6240e72d6305e89ff1711df8cc70f7aa76fb4
Harshil Malhotra <malhotraharshil2@gmail.com>
2024-07-11 11:51:48 +0530

    Fixed Axios

--- src/routes/Jobs.jsx
+++ src/routes/Jobs.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"; // Combine useState and useEffect imports
 import { Link } from 'react-router-dom';
-import { useEffect } from "react";
-export default function Jobs(){
+import axios from 'axios'; // Import axios to make HTTP requests
 
-  const [Jobs, setJobs] = useState([])
+export default function Jobs() {
+  const [jobs, setJobs] = useState([]); // Rename Jobs to jobs to follow naming conventions
 
   useEffect(() => {
     axios.get('http://localhost:5174')
-    .then(result => setJobs(result.data))
-    .catch(err => console.log(err))
-  }, [])
+      .then(result => {
+        // Check if the result.data is an array before setting it to state
+        if (Array.isArray(result.data)) {
+          setJobs(result.data);
+        } else {
+          console.error('Fetched data is not an array:', result.data);
+          setJobs([]); // Default to an empty array if data is not an array
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        setJobs([]); // Default to an empty array in case of error
+      });
+  }, []);
 
   return (
     <>
@@ -17,46 +28,42 @@ export default function Jobs(){
         <div className='w-100 bg-white rounded p-3'>
           <Link to="/CreateJob" className="btn btn-success">Add + </Link>
           <table className="table">
-            <thread>
+            <thead> {/* Correct typo from <thread> to <thead> */}
               <tr>
-                <th> Job</th>
+                <th>Job</th>
                 <th>Company</th>
                 <th>Location</th>
-                <th>Job_Offer</th>
-                <th>Start_Date</th>
+                <th>Job Offer</th> {/* Remove underscore for consistency */}
+                <th>Start Date</th> {/* Remove underscore for consistency */}
                 <th>Openings</th>
                 <th>Action</th>
-                
               </tr>
-            </thread>
+            </thead>
             <tbody>
               {
-                Jobs.map((Job) => {
-                  return <tr>
-                    <td>{Job.Job}</td>
-                    <td>{Job.Company}</td>
-                    <td>{Job.Location}</td>
-                    <td>{Job.Job_Offer}</td>
-                    <td>{Job.Start_Date}</td>
-                    <td>{Job.Openings}</td>
-                    <td>
-                      <Link to="/UpdateJob" className="btn btn-success">Update</Link>
-                      <button className="btn btn-danger">
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
+                jobs.map((job, index) => { // Rename Job to job and add index for key prop
+                  return (
+                    <tr key={index}> {/* Add key prop */}
+                      <td>{job.Job}</td>
+                      <td>{job.Company}</td>
+                      <td>{job.Location}</td>
+                      <td>{job.Job_Offer}</td>
+                      <td>{job.Start_Date}</td>
+                      <td>{job.Openings}</td>
+                      <td>
+                        <Link to="/UpdateJob" className="btn btn-success">Update</Link>
+                        <button className="btn btn-danger">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
                 })
               }
             </tbody>
           </table>
-          
-          
-
         </div>
-        
       </div>
     </>
-    
-  )
+  );
 }
